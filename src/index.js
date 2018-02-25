3a262d4b28fcd91edfff4138a70de1dfcc48a9eb
Jordan Matelsky <j6k4m8@gmail.com>
2018-02-25 12:20:43 -0500

    Add documentation to token reader

--- src/index.js
+++ src/index.js
@@ -10,20 +10,31 @@ try {
     let tokenFileContents = fs.readFileSync(tokenPath).toString().trim();
 
     try {
+        // Try to parse as JSON, where keys are workspace names
+        let data = JSON.parse(tokenFileContents);
+
+        // If that succeeds, then we need a workspace name
+        // which will be the last commandline arg. If we look
+        // for it and we get back the name of this executable,
+        // then the user forgot the workspace name.
         let wsName = process.argv.slice(-1)[0];
         if (
             wsName.endsWith("slackadaisical") ||
             wsName.endsWith("index.js")
         ) {
-            console.error("If using multi-workspace mode, you must specify a workspace with `slackadaisical [workspacename]`.")
+            console.error(
+                "If using multi-workspace mode, you must specify a workspace " +
+                "with `slackadaisical [workspacename]`."
+            )
             process.exit(1);
         }
 
-        // Try to parse as JSON, where keys are workspace names
-        let data = JSON.parse(tokenFileContents);
+        // If we CAN find that workspace name in the JSON,
+        // then set it as the token for this session.
         if (!!data[wsName]) {
             token = data[wsName];
         } else {
+            // Otherwise, warn the user and exit.
             console.log(
                 `Couldn't find workspace [${wsName}] in ~/.slack_token.`
             );
